edd6f37e17f8b63dee9e76427fba4e8243b9348b
Vishesh Singh <VisheshSingh16@gmail.com>
2020-12-05 22:54:32 -0600

    Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
+  <Navbar />
   <router-view />
 </template>
+
+<script>
+import Navbar from './components/Navbar';
+export default {
+  components: { Navbar },
+};
+</script>
 
 <style>
 body {
   background-color: #f2f2f2;
 }
-
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="main-nav">
+    <router-link :to="{ name: 'Home' }">Home</router-link>
+    <router-link :to="{ name: 'AddProject' }">Add a New Project</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+};
+</script>
+
+<style>
+.main-nav {
+  width: 100%;
+  text-align: center;
+  margin: 30px 0;
+}
+
+.main-nav a {
+  padding: 10px;
+  text-decoration: none;
+  /* text-transform: uppercase; */
+  color: #bbb;
+  font-size: 18px;
+}
+
+.main-nav a:not(:first-child) {
+  margin-left: 16px;
+}
+
+.main-nav a.router-link-exact-active {
+  border-bottom: 2px solid #00ce89;
+}
+</style>
