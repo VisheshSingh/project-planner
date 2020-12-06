a0192c7dbd1d079f06017c5191b8c973433ae4ab
Vishesh Singh <VisheshSingh16@gmail.com>
2020-12-05 21:06:44 -0600

    Single Project template

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="project">
+    <div class="actions">
+      <h3 @click="toggleDetails">{{ project.title }}</h3>
+    </div>
+    <div class="details" v-if="showDetails">
+      <p>{{ project.details }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SingleProject',
+  props: ['project'],
+  data() {
+    return {
+      showDetails: false,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.showDetails = !this.showDetails;
+    },
+  },
+};
+</script>
+
+<style>
+.project {
+  margin: 20px auto;
+  padding: 10px 20px;
+  background: #fff;
+  box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.05);
+  border-left: 4px solid #e90074;
+  border-radius: 4px;
+}
+
+h3 {
+  cursor: pointer;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,17 @@
 <template>
   <div v-if="projects.length">
     <div v-for="project in projects" :key="project.id">
-      <h2>{{ project.title }}</h2>
+      <SingleProject :project="project" />
     </div>
   </div>
 </template>
 
 <script>
+import SingleProject from '../components/SingleProject.vue';
+
 export default {
   name: 'Home',
-  components: {},
+  components: { SingleProject },
   data() {
     return {
       projects: [],
