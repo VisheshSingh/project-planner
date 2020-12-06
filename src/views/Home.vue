9320941153509de463075b88d94363e398b90d11
Vishesh Singh <VisheshSingh16@gmail.com>
2020-12-05 20:38:50 -0600

    Setup Json-server

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="home">
-    Home
+  <div v-if="projects.length">
+    <div v-for="project in projects" :key="project.id">
+      <h2>{{ project.title }}</h2>
+    </div>
   </div>
 </template>
 
@@ -8,5 +10,18 @@
 export default {
   name: 'Home',
   components: {},
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  mounted() {
+    const fetchProjects = async () => {
+      const res = await fetch('http://localhost:3000/projects');
+      const data = await res.json();
+      this.projects = data;
+    };
+    fetchProjects();
+  },
 };
 </script>
